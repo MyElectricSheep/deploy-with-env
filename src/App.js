e6364e549c02634b0e5c470e12b0b372eac7f18a
Ben Paget <ben.paget@live.fr>
2021-02-23 14:01:06 +0000

    Deploy

--- src/App.js
+++ src/App.js
@@ -2,21 +2,19 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  console.log(process.env)
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Process.env object printed below:</h1>
+        <div>
+          {Object.keys(process.env).map(key => {
+            return (
+              <p>{key}: {process.env[key]}</p>
+            )
+          })}
+        </div>
       </header>
     </div>
   );
